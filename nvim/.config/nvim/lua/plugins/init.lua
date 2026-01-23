0d56f58bfa2f92906150a0b66fcec563e5168bc0
Gavin Douch <gjdouch@me.com>
2026-01-24 02:31:52 +1100

    Make rainbow indents characterless

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -189,16 +189,20 @@ return {
       }
       local hooks = require("ibl.hooks")
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowRed",    { bg = "#4f2629" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { bg = "#4a3f28" })
+        vim.api.nvim_set_hl(0, "RainbowBlue",   { bg = "#1f324a" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { bg = "#4a3723" })
+        vim.api.nvim_set_hl(0, "RainbowGreen",  { bg = "#263f32" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { bg = "#3f274d" })
+        vim.api.nvim_set_hl(0, "RainbowCyan",   { bg = "#1e3f43" })
       end)
       require("ibl").setup({
-        indent = { highlight = highlight },
+        indent = { highlight = highlight, char = '' },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
         scope = { enabled = false },
       })
     end,
